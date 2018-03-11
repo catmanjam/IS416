535ca0a734d6c0a5cd1a6510f0fae531ef31581c
KAILI <>
2018-03-11 14:45:40 +0800

    Set up walk activity onclick activity

--- app/src/main/java/is416/is416/MainActivity.java
+++ app/src/main/java/is416/is416/MainActivity.java
@@ -1,7 +1,9 @@
 package is416.is416;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void startWalkActivity(View v) {
+        Intent launchWalk = new Intent(this, WalkActivity.class);
+        startActivity(launchWalk);
     }
 }
