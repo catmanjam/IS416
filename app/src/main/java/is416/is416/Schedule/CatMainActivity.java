123df6f3f2b69e39200b2c2d52e92230e65bda0a
Cheryl Lim <cheryl.lim.2015@smu.edu.sg>
2018-03-24 11:53:34 +0800

    Update main page

--- app/src/main/java/is416/is416/Schedule/CatMainActivity.java
+++ app/src/main/java/is416/is416/Schedule/CatMainActivity.java
@@ -1,9 +1,13 @@
 package is416.is416.Schedule;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import is416.is416.GameActivity;
 import is416.is416.R;
+import is416.is416.WalkActivity;
 
 public class CatMainActivity extends AppCompatActivity {
 
@@ -11,5 +15,20 @@ public class CatMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_main);
+    }
+
+    public void goTask(View view) {
+        Intent it = new Intent(this, ScheduleActivity.class);
+        startActivity(it);
+    }
+
+    public void goWalk(View view) {
+        Intent walkLaunch = new Intent(this, WalkActivity.class);
+        startActivity(walkLaunch);
+    }
+
+    public void goPlay(View view) {
+        Intent gameLaunch = new Intent(this, GameActivity.class);
+        startActivity(gameLaunch);
     }
 }
