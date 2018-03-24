e27b700f1cb849a60e9035e8de877a555a99bf05
Cheryl Lim <cheryl.lim.2015@smu.edu.sg>
2018-03-24 20:19:09 +0800

    added mic function to main activity

--- app/src/main/java/is416/is416/CatMainActivity.java
+++ app/src/main/java/is416/is416/CatMainActivity.java
@@ -1,21 +1,55 @@
 package is416.is416;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import is416.is416.Database.Database;
 import is416.is416.GameActivity;
 import is416.is416.R;
+import is416.is416.Schedule.DialogueFlowActivity;
+import is416.is416.Schedule.Mic;
 import is416.is416.Schedule.ScheduleActivity;
 import is416.is416.WalkActivity;
 
 public class CatMainActivity extends AppCompatActivity {
 
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 101;
+    private Mic mic;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_main);
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        Button micButton = (Button) findViewById(R.id.micButton);
+        TextView questionView = (TextView) findViewById(R.id.question);
+        TextView answerView = (TextView) findViewById(R.id.answer);
+
+        mic = new Mic(this, questionView, answerView);
+        mic.micStart();
     }
 
     public void goTask(View view) {
@@ -31,5 +65,9 @@ public class CatMainActivity extends AppCompatActivity {
     public void goPlay(View view) {
         Intent gameLaunch = new Intent(this, GameActivity.class);
         startActivity(gameLaunch);
+    }
+
+    public void micClick(View view) {
+        mic.micStartListening();
     }
 }
--- app/src/main/java/is416/is416/Schedule/Mic.java
+++ app/src/main/java/is416/is416/Schedule/Mic.java
@@ -1,18 +1,11 @@
 package is416.is416.Schedule;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-
 import android.Manifest;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +13,8 @@ import com.google.gson.JsonElement;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -27,6 +22,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
+
 import ai.api.AIListener;
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
@@ -35,103 +31,37 @@ import ai.api.model.AIResponse;
 import ai.api.model.Result;
 import is416.is416.Database.Appointment;
 import is416.is416.Database.Database;
-import is416.is416.R;
 
+/**
+ * Created by Cheryl on 24/3/2018.
+ */
 
-public class DialogueFlowActivity extends AppCompatActivity implements AIListener {
+public class Mic implements AIListener {
     private AIService aiService;
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 101;
     private static String CLIENT_ACCESS_TOKEN = "4ae9328c789d41af97d6f14d558dccab";
     private static String TAG = "Permission";
     private Database myDb;
-    private Button listenButton;
-    TextView resultTextView, outputText;
+    private Context context;
+    private TextView questionView;
+    private TextView answerView;
 
-    // Requesting permission to RECORD_AUDIO
-    private boolean permissionToRecordAccepted = false;
-    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        myDb= Database.getInstance(this);
-        switch (requestCode){
-            case REQUEST_RECORD_AUDIO_PERMISSION:
-                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                break;
-        }
-        if (!permissionToRecordAccepted ) finish();
-
+    public Mic(Context context, TextView questionView, TextView answerView){
+        this.questionView = questionView;
+        this.answerView = answerView;
+        this.context = context;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dialogue_flow);
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-        listenButton = (Button) findViewById(R.id.listenButton);
-        resultTextView = (TextView) findViewById(R.id.resultTextView);
-        outputText = (TextView) findViewById(R.id.outputText);
-
+    public void micStart(){
         final AIConfiguration config = new AIConfiguration(CLIENT_ACCESS_TOKEN,
                 AIConfiguration.SupportedLanguages.English,
                 AIConfiguration.RecognitionEngine.System);
-        aiService = AIService.getService(this, config);
+        aiService = AIService.getService(context, config);
         aiService.setListener(this);
+        myDb = Database.getInstance(context);
     }
 
-
-
-    @Override
-    public void onResult(final AIResponse response) {
-        Result result = response.getResult();
-
-        // Get parameters
-        String parameterString = "";
-        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
-            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
-                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
-            }
-        }
-        String userQuery = result.getResolvedQuery();
-        // Show results in TextView.
-        resultTextView.setText("Query:" + userQuery + "\n"
-                + "Parameter: " + parameterString);
-        RetrieveFeedTask task=new RetrieveFeedTask();
-        task.execute(userQuery);
-
-    }
-
-    @Override
-    public void onError(AIError error) {
-        resultTextView.setText(error.toString());
-    }
-
-    @Override
-    public void onAudioLevel(float level) {
-
-    }
-
-    @Override
-    public void onListeningStarted() {
-
-    }
-
-    @Override
-    public void onListeningCanceled() {
-
-    }
-
-    @Override
-    public void onListeningFinished() {
-
-    }
-
-    public void listenButtonClicked(View view) {
-        Log.d("Listening", "ok");
+    public void micStartListening(){
         aiService.startListening();
-
     }
 
     public String GetText(String query) throws UnsupportedEncodingException {
@@ -159,16 +89,12 @@ public class DialogueFlowActivity extends AppCompatActivity implements AIListene
             JSONArray queryArray = new JSONArray();
             queryArray.put(query);
             jsonParam.put("query", queryArray);
-//            jsonParam.put("name", "order a medium pizza");
             jsonParam.put("lang", "en");
             jsonParam.put("sessionId", "1234567890");
 
-
             OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            Log.d("karma", "after conversion is " + jsonParam.toString());
             wr.write(jsonParam.toString());
             wr.flush();
-            Log.d("karma", "json is " + jsonParam);
 
             // Get the server response
 
@@ -176,17 +102,13 @@ public class DialogueFlowActivity extends AppCompatActivity implements AIListene
             StringBuilder sb = new StringBuilder();
             String line = null;
 
-
             // Read Server Response
             while ((line = reader.readLine()) != null) {
                 // Append server response in string
                 sb.append(line + "\n");
             }
 
-
             text = sb.toString();
-
-
 
             JSONObject object1 = new JSONObject(text);
             JSONObject result = object1.getJSONObject("result");
@@ -202,7 +124,7 @@ public class DialogueFlowActivity extends AppCompatActivity implements AIListene
 
             if (time!= null && !time.isEmpty() && name != null && !name.isEmpty() && date != null && !date.isEmpty()){
                 myDb.addAppointment(new Appointment(date, time, name));
-                Toast.makeText(this, "Task Added", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Item added", Toast.LENGTH_SHORT).show();
             }
 
             Log.d("c ", "response is " + text);
@@ -217,13 +139,26 @@ public class DialogueFlowActivity extends AppCompatActivity implements AIListene
             } catch (Exception ex) {
             }
         }
-
         return null;
     }
 
-    public void goApptList(View view) {
-        Intent it = new Intent(this, ScheduleActivity.class);
-        startActivity(it);
+    @Override
+    public void onResult(final AIResponse response) {
+        Result result = response.getResult();
+
+        // Get parameters
+        String parameterString = "";
+        if (result.getParameters() != null && !result.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+        String userQuery = result.getResolvedQuery();
+        // Show results in TextView.
+        questionView.setText("Query:" + userQuery + "\n"
+                + "Parameter: " + parameterString);
+        RetrieveFeedTask task=new RetrieveFeedTask();
+        task.execute(userQuery);
     }
 
     class RetrieveFeedTask extends AsyncTask<String, Void, String> {
@@ -247,8 +182,34 @@ public class DialogueFlowActivity extends AppCompatActivity implements AIListene
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            outputText.setText(s);
+            answerView.setText(s);
 
         }
+    }
+
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
