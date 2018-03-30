b6ae19b29ffa514036aba3584e51792fd762bb41
Tan Jun Ming <jmtan.2015@smu.edu.sg>
2018-03-30 15:29:17 +0800

    updated game added gif

--- app/src/main/java/is416/is416/GameActivity.java
+++ app/src/main/java/is416/is416/GameActivity.java
@@ -1,6 +1,7 @@
 package is416.is416;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -46,13 +47,23 @@ public class GameActivity extends AppCompatActivity {
     private Bitmap cross;
     private Bitmap cat;
     private int score = 0;
+    private int highScore = 0;
 
     final Random random = new Random();
     final Point size = new Point();
 
+    private TextView gameOver;
+    private TextView tryAgain;
+    private Button retry;
+    private Button toHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_game);
         //l = (RelativeLayout) findViewById(R.id.layout);
@@ -69,7 +80,7 @@ public class GameActivity extends AppCompatActivity {
         Display display = getWindowManager().getDefaultDisplay();
         display.getSize(size);
         xMax = (float) size.x - 150;
-        yMax = (float) size.y - 460;
+        yMax = (float) size.y - 300;
 
         //final Random random = new Random();
         xRand = (float) random.nextInt(size.x-150);
@@ -154,33 +165,57 @@ public class GameActivity extends AppCompatActivity {
         mp.pause();
     }
 
+    public void goHome (View view) {
+        Intent homeLaunch = new Intent(this, CatMainActivity.class);
+        startActivity(homeLaunch);
+    }
+
     public void startGame(View view){
-        l = (RelativeLayout) findViewById(R.id.layout);
-        GameView gameView = new GameView(this);
-        l.addView(gameView);
         TextView gameTitle = findViewById(R.id.gameTitle);
-        ImageView catBg = (ImageView) findViewById(R.id.catbg);
-        TextView howToPlay = (TextView) findViewById(R.id.howtoplay);
-        Button gameStart = (Button) findViewById(R.id.gameStart);
-        //GridLayout howTo = findViewById(R.id.howto);
+        ImageView catBg = findViewById(R.id.catbg);
+        TextView howToPlay = findViewById(R.id.howtoplay);
+        Button gameStart = findViewById(R.id.gameStart);
+        ImageView howTo = findViewById(R.id.howto);
         gameTitle.setVisibility(View.INVISIBLE);
         catBg.setVisibility(View.INVISIBLE);
         howToPlay.setVisibility(View.INVISIBLE);
         gameStart.setVisibility(View.INVISIBLE);
-        //howTo.setVisibility(View.INVISIBLE);
+        howTo.setVisibility(View.INVISIBLE);
+
+        gameOver = findViewById(R.id.gameoverText);
+        tryAgain = findViewById(R.id.gameoverText2);
+        retry = findViewById(R.id.replay);
+        toHome = findViewById(R.id.returnHome);
+
+        gameOver.setVisibility(View.INVISIBLE);
+        tryAgain.setVisibility(View.INVISIBLE);
+        retry.setVisibility(View.INVISIBLE);
+        toHome.setVisibility(View.INVISIBLE);
+
+        score = 0;
+
+
+        l = findViewById(R.id.layout);
+        GameView gameView = new GameView(this,view);
+        l.addView(gameView);
     }
+
 
     private class GameView extends View {
 
-        public GameView(Context context) {
+        private View view;
+        private Context context;
+
+        public GameView(Context context,View view) {
             super(context);
+            this.context = context;
 
             final int catWidth = 200;
             final int catHeight = 200;
             final int ballWidth = 100;
             final int ballHeight = 100;
-            final int crossWidth = 80;
-            final int crossHeight = 80;
+            final int crossWidth = 100;
+            final int crossHeight = 100;
 
             Bitmap catSrc = BitmapFactory.decodeResource(getResources(), R.drawable.catgame);
             cat = Bitmap.createScaledBitmap(catSrc, catWidth, catHeight, true);
@@ -196,16 +231,27 @@ public class GameActivity extends AppCompatActivity {
         @Override
         protected void onDraw(Canvas canvas) {
 
+            gameOver = ((GameActivity)context).findViewById(R.id.gameoverText);
+            tryAgain = ((GameActivity)context).findViewById(R.id.gameoverText2);
+            retry = ((GameActivity)context).findViewById(R.id.replay);
+            toHome = ((GameActivity)context).findViewById(R.id.returnHome);
+            l = ((GameActivity)context).findViewById(R.id.layout);
+
             Paint p = new Paint();
-            p.setTextSize(100);
+            p.setTextSize(90);
             p.setColor(Color.parseColor("#4b4b4b"));
             p.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-            canvas.drawText("Score: " + score,540,250,p);
+            canvas.drawText("High Score: " + highScore,120,250,p);
+            canvas.drawText("Score: " + score,990,250,p);
+
             canvas.drawBitmap(cat, xPos, yPos, null);
             canvas.drawBitmap(ball,xRand,yRand, null);
             canvas.drawBitmap(cross,xRandx,yRandx,null);
             if ((xPos >= xRand-100f && xPos <= xRand+100f) && (yPos >= yRand-100f && yPos <= yRand+100f)) {
                 score++;
+                if (score > highScore) {
+                    highScore = score;
+                }
                 xRand = (float) random.nextInt(size.x-150);
                 yRand = (float) random.nextInt(size.y-460);
                 xRandx = (float) random.nextInt(size.x-150);
@@ -215,6 +261,18 @@ public class GameActivity extends AppCompatActivity {
 
 
 
+            }
+            if ((xPos >= xRandx-100f && xPos <= xRandx+100f) && (yPos >= yRandx-100f && yPos <= yRandx+100f)) {
+                xRand = (float) random.nextInt(size.x-150);
+                yRand = (float) random.nextInt(size.y-460);
+                xRandx = (float) random.nextInt(size.x-150);
+                yRandx = (float) random.nextInt(size.y-460);
+
+                gameOver.setVisibility(View.VISIBLE);
+                tryAgain.setVisibility(View.VISIBLE);
+                retry.setVisibility(View.VISIBLE);
+                toHome.setVisibility(View.VISIBLE);
+                l.removeView(this);
             }
             invalidate();
         }
